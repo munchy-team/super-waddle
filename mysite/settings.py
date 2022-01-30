7ca503716067459130cf3b1d24e765c60433399b
coding235429878 <>
2022-01-30 04:26:15 +0000

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'

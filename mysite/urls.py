ffdfd0971fea108c532e04d04bd58b535cd6130b
cs947939 <40151447+cs947939@users.noreply.github.com>
2022-01-31 16:33:03 +0000

    testing 123

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from mysite import settings
 
 # from progress.views import HttpResponse
 
@@ -27,6 +28,7 @@ from . import views
 
 
 admin.site.site_header= "Project Progress Website"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

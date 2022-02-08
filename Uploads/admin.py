68eda7a9a44cb87e90675a42c7238c5d06eda8f0
coding235429878 <97757073+coding986532@users.noreply.github.com>
2022-02-08 02:15:17 +0000

    update again

--- Uploads/admin.py
+++ Uploads/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import File
-# document is the model
 
-class FileAdmin(admin.ModelAdmin):
-    list_display = ('Title', 'uploaded_at')
+
+#class FileAdmin(admin.ModelAdmin):
+  #  list_display = ('Title', 'uploaded_at')
 
 # Register your models here.
-admin.site.register(File, FileAdmin)
+#admin.site.register(File, FileAdmin)
 
 # Register your models here.
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-\
+
 
 
 """mysite URL Configuration

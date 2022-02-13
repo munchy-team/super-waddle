c493c9bad47600e210b1aaa157c115100463c81e
coding986532 <munchyjunk10@outlook.com>
2022-02-13 13:30:16 -0800

    updating views

--- mysite/views.py
+++ mysite/views.py
@@ -42,7 +42,7 @@ def test(request):
 def navigation(request):
 
   #html = '<html> <title>Munchy-Site | Navigation</title> <h2>Navigation</h2><p>Or just go back to the <a href="/admin">log in</a> or <a href="/">home</a> page. </p><h3>/Progress</h3> <ul> <li><a href="#">Link 1</a> </li> <li><a href="#">Link 2</a> </li><li><a href="#">Link 3</a> </li></ul> </html>'
-  return HttpResponse(html)
+  return HttpResponse()
 def main(request):
     documents = Document.objects.all()
     return render(request, 'uploader/main.html', { 'documents': documents })

f5d9161b5c11ba776c2614c6eeee878bd81972ee
cs947939 <40151447+cs947939@users.noreply.github.com>
2022-03-02 19:27:34 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
+  #  'allauth.account.auth_backends.AuthenticationBackend',
   
 ]
 
@@ -162,7 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
-SITE_ID = 1
+
 
 #SOCIALACCOUNT_PROVIDERS = {
 

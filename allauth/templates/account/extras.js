8fa1ecdbc53e32eee1a0d71383f88c9747f7d0b2
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2022-03-05 20:45:33 -0600

    html changes

---
+++ allauth/templates/account/extras.js
@@ -0,0 +1,26 @@
+/* 
+     <p>
+                <center>
+                    {% blocktrans with site.name as site_name %}Please sign in with one
+                    of your existing third party accounts. Or, <a href="{{ signup_url }}">click here</a>
+                    to sign up for a {{ site_name }} account and sign in below:{% endblocktrans %}
+                </center>
+                <hr>
+            </p>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */

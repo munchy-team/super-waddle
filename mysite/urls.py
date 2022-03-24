a632937ddd21d04243e819a726f84d58e7bf0455
cs947939 <40151447+cs947939@users.noreply.github.com>
2022-03-24 20:43:19 +0000

    sorry rick rolls today

--- mysite/urls.py
+++ mysite/urls.py
@@ -82,6 +82,7 @@ urlpatterns = [
   path('CSerror', views.CSRF_ERROR),
   path('munchy2', TemplateView.as_view(template_name="logon2.html")),
   path('munchy3', TemplateView.as_view(template_name="logon3.html")),
+    path('vbucks', TemplateView.as_view(template_name="rickroll.html")),
   path('blogs/', include('blog.urls')),
   # path('accountss/', include('accountss.urls')),
    # url(r'^upload-tool/$', views.main, name='main'),

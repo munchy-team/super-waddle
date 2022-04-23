9b7707b6bf88a8fb9124efb8c040846a10bc49f7
cs947939 <sufam5@outlook.com>
2022-04-22 19:28:24 -0500

    New allauth

--- allauth/socialaccount/urls.py
+++ allauth/socialaccount/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
         name="socialaccount_login_cancelled",
     ),
     path("login/error/", views.login_error, name="socialaccount_login_error"),
-   # path("signup/", views.signup, name="socialaccount_signup"),
+    path("signup/", views.signup, name="socialaccount_signup"),
     path("connections/", views.connections, name="socialaccount_connections"),
 ]

2e9bc4eb3157542c82d499e6901dff1ef2f9ce2e
cs947939 <40151447+cs947939@users.noreply.github.com>
2022-08-02 13:56:34 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,7 +171,7 @@ STATICFILES_FINDERS = (
  )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '.well-known')#'media'
-MEDIA_URL = '/.well-known/' #real one - #MEDIA_URL = '/media/'
+MEDIA_URL = "/.well-known/" #real one - #MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 SITE_ID = 2

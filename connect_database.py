4765153508cabe34ecc236651c772e295a50e01e
coding986532 <97757073+coding986532@users.noreply.github.com>
2022-08-27 14:57:08 -0500

    push

--- connect_database.py
+++ connect_database.py
@@ -2,7 +2,7 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
 cloud_config= {
-        'secure_connect_bundle': '<</PATH/TO/>>secure-connect-testing-1.zip'
+        'secure_connect_bundle': 'secure-connect-testing-1.zip'
 }
 auth_provider = PlainTextAuthProvider('LBowjsOjnzbmhabzUTvvojxN', 'GQADoHtukd5ULJUM_pSvRqg_ZB8xtri11lZfx6_HFvDWzWYYlpK5Xq.lw5BIIrwqZ-SmcLZtEzC,jYyiGdJXjdCGM2-JZX0B,OOPQps460zcPm-5T_M_Ceubvmau0BfT')
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
@@ -23,4 +23,5 @@ cloud_config= {
 auth_provider = PlainTextAuthProvider('LBowjsOjnzbmhabzUTvvojxN', 'GQADoHtukd5ULJUM_pSvRqg_ZB8xtri11lZfx6_HFvDWzWYYlpK5Xq.lw5BIIrwqZ-SmcLZtEzC,jYyiGdJXjdCGM2-JZX0B,OOPQps460zcPm-5T_M_Ceubvmau0BfT')
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
 session = cluster.connect()
+
 '''
